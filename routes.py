fe06b8a8ed96309a5daf615d165a1eed0af476a2
Subhasmita Nayak <23f3003549@ds.study.iitm.ac.in>
2025-03-09 11:53:09 +0530

    basic login and register functionality

--- routes.py
+++ routes.py
@@ -1,6 +1,75 @@
-from flask import render_template
+from flask import render_template,request,url_for,flash,redirect
 from app import app
+from datetime import datetime
+from models import db, User,Subject,Quiz,Question,Chapter
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/', methods=['POST'])
+def ind_post():
+    username=request.form.get('username')
+    password=request.form.get('password')
+    return "username:"+username+" password:"+ password
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/login', methods=['POST'])
+def login_post():
+    username=request.form.get('username')
+    password=request.form.get('password')
+
+    if not username or not password:
+        flash("Enter username and password")
+
+    user=User.query.filter_by(username=username).first()  
+
+    if not user:
+        flash('Username does not exists') 
+        return redirect(url_for('login'))
+    if not check_password_hash(user.passhash, password):
+        flash('Incorrect password')
+        return redirect(url_for('login'))
+    
+    return "Successfully login"
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+    
+@app.route('/register', methods=['POST'])
+def register_post():
+    username=request.form.get('username')
+    password=request.form.get('password')
+    c_password=request.form.get('password1')
+    fullname=request.form.get('fullname')
+    qualification=request.form.get('qualification')
+    dob=request.form.get('dob')
+    date_object=datetime.strptime(dob,'%Y-%m-%d')
+    
+    if not username or not password or not c_password:
+        flash("please fill out these fields")
+        return redirect(url_for('register'))
+    if password!=c_password:
+        flash("Password do not match ")
+        return redirect(url_for('register'))
+    user = User.query.filter_by(username=username).first()
+    if user:
+        flash('Username is already exists')
+        return redirect(url_for('register'))
+    
+    
+    
+
+    password_hash=generate_password_hash(password)
+
+    new_user = User(username=username, passhash=password_hash, fullName=fullname, qualification=qualification, dob=date_object)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('login'))
+
+
